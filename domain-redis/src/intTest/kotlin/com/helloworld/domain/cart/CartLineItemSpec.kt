65e6438c01a59ef65d7f20192247523b7a15ba73
Jiwhun Kim <jiwhun.kim@outlook.com>
2021-09-15 20:57:11 +0900

    integrationTest 변경

---
+++ domain-redis/src/intTest/kotlin/com/helloworld/domain/cart/CartLineItemSpec.kt
@@ -0,0 +1,48 @@
+package com.helloworld.domain.cart
+
+import com.helloworld.config.redis.config.RedisConfig
+import com.helloworld.order.domain.CartLineItem
+import com.helloworld.order.domain.CartProduct
+import com.helloworld.order.domain.CartProductOption
+import com.helloworld.order.domain.CartSellerProduct
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.comparables.shouldBeEqualComparingTo
+import org.springframework.boot.test.autoconfigure.data.redis.DataRedisTest
+import org.springframework.context.annotation.Import
+import org.springframework.test.context.ActiveProfiles
+import java.math.BigDecimal
+
+@DataRedisTest
+@Import(RedisConfig::class)
+@ActiveProfiles("test")
+class CartLineItemSpec : DescribeSpec() {
+    init {
+        describe("cartLineItem") {
+            it("create cartLineItem test") {
+                val cartProduct = CartProduct(productId = 1L, productName = "productName")
+                val cartProductOption = CartProductOption(
+                    productOptionId = 1L,
+                    productOptionName = "productOptionName",
+                    salesAmount = BigDecimal(1100),
+                    discountAmount = BigDecimal(100),
+                    amount = BigDecimal(1000)
+                )
+                val cartSellerProduct = CartSellerProduct(
+                    sellerProductId = 1L,
+                    sellerProductName = "sellerProductName",
+                    salesAmount = BigDecimal(1100),
+                    discountAmount = BigDecimal(100),
+                    amount = BigDecimal(1000)
+                )
+                val cartLineItem = CartLineItem(
+                    cartProduct = cartProduct,
+                    cartProductOption = cartProductOption,
+                    cartSellerProduct = cartSellerProduct
+                )
+                cartLineItem.amount.shouldBeEqualComparingTo(BigDecimal(1000))
+                cartLineItem.quantity = 2
+                cartLineItem.amount.shouldBeEqualComparingTo(BigDecimal(2000))
+            }
+        }
+    }
+}
---
+++ domain-redis/src/intTest/kotlin/com/helloworld/domain/cart/CartSpec.kt
@@ -0,0 +1,64 @@
+package com.helloworld.domain.cart
+
+import com.helloworld.config.redis.config.RedisConfig
+import com.helloworld.order.domain.*
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+import org.springframework.boot.test.autoconfigure.data.redis.DataRedisTest
+import org.springframework.context.annotation.Import
+import org.springframework.test.context.ActiveProfiles
+import java.math.BigDecimal
+
+@DataRedisTest
+@Import(RedisConfig::class)
+@ActiveProfiles("test")
+class CartSpec(cartRepository: CartRepository) : DescribeSpec() {
+    init {
+        describe("cart") {
+            it("create cart no list") {
+                val cart = Cart(1L)
+                cart.id.shouldNotBe(null)
+            }
+            it("create cart with empty list") {
+                val cart = Cart(1L, emptyList())
+                cart.id.shouldNotBe(null)
+                cart.accountId.shouldBe(1L)
+            }
+
+            it("create cart with list") {
+                val cartProduct = CartProduct(productId = 1L, productName = "productName")
+                val cartProductOption = CartProductOption(
+                    productOptionId = 1L,
+                    productOptionName = "productOptionName",
+                    salesAmount = BigDecimal(1100),
+                    discountAmount = BigDecimal(100),
+                    amount = BigDecimal(1000)
+                )
+                val cartSellerProduct = CartSellerProduct(
+                    sellerProductId = 1L,
+                    sellerProductName = "sellerProductName",
+                    salesAmount = BigDecimal(1100),
+                    discountAmount = BigDecimal(100),
+                    amount = BigDecimal(1000)
+                )
+                val cartLineItem = CartLineItem(
+                    cartProduct = cartProduct,
+                    cartProductOption = cartProductOption,
+                    cartSellerProduct = cartSellerProduct
+                )
+                val cartLineItem2 = CartLineItem(
+                    cartProduct = cartProduct,
+                    cartProductOption = cartProductOption,
+                    cartSellerProduct = cartSellerProduct
+                )
+
+                val cart = Cart(1L, listOf(cartLineItem, cartLineItem2))
+                val result = cartRepository.save(cart)
+                result.salesAmount.shouldBe(cartLineItem.salesAmount)
+                result.discountAmount.shouldBe(cartLineItem.discountAmount)
+                result.amount.shouldBe(cartLineItem.amount)
+            }
+        }
+    }
+}
